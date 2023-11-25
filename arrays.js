581b2e2af793c5601d9b4047332ef133460f60af
Jacob <jrogergordon@gmail.com>
2023-11-24 23:17:13 -0500

    122, best time to buy and sell stock 2

--- arrays.js
+++ arrays.js
@@ -30,11 +30,11 @@ var maxProfit = function (prices) {
 
 // You are given an array prices where prices[i] is the price of a given stock on the ith day.
 
-You want to maximize your profit by choosing a single day to buy one stock and choosing a 
-different day in the future to sell that stock.
+// You want to maximize your profit by choosing a single day to buy one stock and choosing a 
+// different day in the future to sell that stock.
 
-Return the maximum profit you can achieve from this transaction.If you cannot achieve any profit,
- return 0.
+// Return the maximum profit you can achieve from this transaction.If you cannot achieve any profit,
+//  return 0.
 
 
 var maxProfit = function (prices) {
@@ -49,4 +49,25 @@ var maxProfit = function (prices) {
         }
     }
     return max;
+};
+
+
+
+// You are given an integer array prices where prices[i] is the price of a given stock on the ith day.
+
+// On each day, you may decide to buy and / or sell the stock.You can only hold at most one share of the stock at any time.However, you can buy it then immediately sell it on the same day.
+
+// Find and return the maximum profit you can achieve.
+var maxProfit = function (prices) {
+    let sum = 0;
+
+    for (let i = 1; i < prices.length; i++) {
+        if (prices[i - 1] < prices[i]) {
+            sum = sum + prices[i] - prices[i - 1];
+        }
+    }
+
+    return sum;
+
+
 };
