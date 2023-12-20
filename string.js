acfca835f7db7c74b97879aad963d0535e47bb25
Jacob <jrogergordon@gmail.com>
2023-12-20 13:07:33 -0500

    468. Validate IP Address

--- string.js
+++ string.js
@@ -234,110 +234,57 @@ var findRepeatedDnaSequences = function (s) {
 };
 
 
+Given a string queryIP, return "IPv4" if IP is a valid IPv4 address, "IPv6" if IP is a valid IPv6 address or "Neither" if IP is not a correct IP of any type.
 
-/**
- * @param {string} queryIP
- * @return {string}
- */
+A valid IPv4 address is an IP in the form "x1.x2.x3.x4" where 0 <= xi <= 255 and xi 
+cannot contain leading zeros.For example, "192.168.1.1" and "192.168.1.0" 
+are valid IPv4 addresses while "192.168.01.1", "192.168.1.00", and "192.168@1.1" 
+are invalid IPv4 addresses.
 var validIPAddress = function (queryIP) {
     let nums = new Set();
     let v6 = new Set();
     nums.add("0"); nums.add("1"); nums.add("2"); nums.add("3"); nums.add("4"); nums.add("5"); nums.add("6"); nums.add("7");
     nums.add("8"); nums.add("9");
 
-    v6.add("a"); v6.add("b"); v6.add("c"); v6.add("d"); v6.add("e"); v6.add("f"); v6.add("g"); v6.add("h"); v6.add("i"); v6.add("j");
-    v6.add("k"); v6.add("l"); v6.add("m"); v6.add("n"); v6.add("o"); v6.add("p"); v6.add("q"); v6.add("r"); v6.add("s"); v6.add("t");
-    v6.add("u"); v6.add("v"); v6.add("w"); v6.add("x"); v6.add("y"); v6.add("z"); v6.add("A"); v6.add("B"); v6.add("C"); v6.add("D");
-    v6.add("E"); v6.add("F"); v6.add("G"); v6.add("H"); v6.add("I"); v6.add("J"); v6.add("K"); v6.add("L"); v6.add("M"); v6.add("N");
-    v6.add("O"); v6.add("P"); v6.add("Q"); v6.add("R"); v6.add("S"); v6.add("T"); v6.add("U"); v6.add("V"); v6.add("W"); v6.add("X");
-    v6.add("Y"); v6.add("Z");
+    v6.add("a"); v6.add("b"); v6.add("c"); v6.add("d"); v6.add("e"); v6.add("f"); v6.add("A"); v6.add("B"); v6.add("C"); v6.add("D");
+    v6.add("E"); v6.add("F");; v6.add("0"); v6.add("1"); v6.add("2"); v6.add("3"); v6.add("4"); v6.add("5"); v6.add("6");
+    v6.add("7"); v6.add("8"); v6.add("9");
 
     function valid4(text) {
         let count = 0;
         let i = 0;
-        while (i < text.length) {
-            console.log(i, "hi");
-            let curr = "";
-            if (text[i] === "0" && text[i + 1] !== "." && text[i + 1]) {
-                console.log("yeet")
-                i = text.length;
-                return "Neither"
-            }
-            while (text[i] !== "." && text[i]) {
-                if (!nums.has(text[i]) && text[i]) {
-                    console.log("meat")
-                    i = text.length;
-                    return "Neither";
-                }
-                curr = curr + text[i]
+        let curr = ""
+        for (let i = 0; i < text.length; i++) {
+            if (text[i] === ".") {
+                count++;
+                if (Number(curr) > 255) return "Neither";
+                curr = ""
                 i++;
             }
-            count++;
-            if (text[i + 1]) {
-                i++;
-            }
-            curr = Number(curr);
-            if (curr > 255 && curr >= 1) {
-                console.log("2")
-                i = text.length;
-                return "Neither";
-            }
-            console.log(i, text[i] === ".", !text[i + 1])
-            if (text[i] === "." && !text[i + 1]) {
-                i++;
-                return "Neither";
-            }
-            if (i === text.length - 1) {
-                i++;
-            }
+            if (curr === "" && text[i] === "0" && nums.has(text[i + 1])) return "Neither";
+            if (!nums.has(text[i])) return "Neither";
+            curr = curr + text[i];
         }
-        if (!count === 4) {
-            return "Neither";
-        }
-        return "IPv4"
+        if (count === 3 && Number(curr) < 256) return "IPv4"
+        return "Neither"
     }
 
     function valid6(text) {
         let count = 0;
         let i = 0;
-        while (i < text.length) {
-            let curr = "";
-            while (text[i] !== ":" && text[i]) {
-                console.log(i);
-                if (!nums.has(text[i]) && !v6.has(text[i]) && text[i]) {
-                    console.log("hit1");
-                    i = text.length;
-                    return "Neither";
-                }
-                if (text[i]) {
-                    curr = curr + text[i]
-                }
-
-                if (curr.length > 4) {
-                    console.log("hit2", curr)
-                    i = text.length;
-                    return 'Neither';
-                }
+        let curr = ""
+        for (let i = 0; i < text.length; i++) {
+            if (text[i] === ":") {
+                count++;
+                if (curr.length > 4) return "Neither"
+                curr = ""
                 i++;
             }
-            count++
-
-            if (text[i] !== ":" && text[i]) {
-                console.log("hit3");
-                i = text.length;
-                return "Neither"
-            } else if (text[i] === ":" && text[i + 1]) {
-                i++;
-            } else if (text[i] === ":" && !text[i + 1]) {
-                i++;
-                return "Neither";
-            }
+            if (!v6.has(text[i])) return "Neither";
+            curr = curr + text[i];
         }
-        if (!count === 8) {
-            return "Neither";
-        }
-        console.log(count)
-        return "IPv6"
+        if (count === 7 && curr.length <= 4) return "IPv6"
+        return "Neither"
     }
 
     let check = false;
