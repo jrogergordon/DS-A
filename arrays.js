8cf933a2f6a325013d9129bcd827fb530949edaf
Jacob <jrogergordon@gmail.com>
2023-12-30 23:52:07 -0500

    2sum

--- arrays.js
+++ arrays.js
@@ -132,6 +132,11 @@ var twoSum = function (nums, target) {
     for (var i = 0; i < nums.length; i++) {
         var n = nums[i];
 
-
+        if (map[target - n] >= 0) {
+            return [map[target - n], i]
+        }
+        else {
+            map[n] = i;   //use map to store the value and index position
+        }
     }
 };
