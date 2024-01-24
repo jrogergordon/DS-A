66d75b36714ab5ab0ccdf711b68e661359bdbf0d
Jacob <jrogergordon@gmail.com>
2024-01-24 15:33:49 -0500

    394. Decode String

--- string.js
+++ string.js
@@ -298,3 +298,88 @@ var validIPAddress = function (queryIP) {
         return valid4(queryIP)
     }
 };
+
+// Given an encoded string, return its decoded string.
+
+// The encoding rule is: k[encoded_string], where the encoded_string inside the square brackets is being repeated exactly k times.Note that k is guaranteed to be a positive integer.
+
+// You may assume that the input string is always valid; there are no extra white spaces, square brackets are well - formed, etc.Furthermore, you may assume that the original data does not contain any digits and that digits are only for those repeat numbers, k.For example, there will not be input like 3a or 2[4].
+
+// The test cases are generated so that the length of the output will never exceed 105.
+
+var decodeString = function(s) {
+    console.log(s)
+    let decoded = "";
+    let nums = new Set();
+    nums.add("0")
+    nums.add("1")
+    nums.add("2")
+    nums.add("3")
+    nums.add("4")
+    nums.add("5")
+    nums.add("6")
+    nums.add("7")
+    nums.add("8")
+    nums.add("9")
+    let wrds = [];
+    let rep = [];
+    let solve = function(strArr, numArr) {
+        console.log(numArr, strArr)
+        let currNum;
+        let currStr;
+        let curr = "";
+        while(strArr.length) {
+            if(!numArr.length) {
+                currNum = 1;               
+            } else {
+                currNum = numArr.pop();
+            }
+            currStr =  strArr.pop() + curr;
+            curr = currStr;
+            while(currNum > 1) {
+                curr = curr + currStr;
+                currNum--;
+            }
+        }
+        return curr;
+    }
+    let openClose = 0;
+    let opClo = new Map();
+    opClo.set("[", 1);
+    opClo.set("]", -1);
+    for(let i = 0; i < s.length; i++) {
+        if(opClo.has(s[i])) {
+            openClose = openClose + opClo.get(s[i])
+        };
+        if(nums.has(s[i])) {
+            let numCurr = "";
+            while(nums.has(s[i])) {           
+                numCurr = numCurr + s[i];
+                i++;
+            }
+            i--;
+            rep.push(parseInt(numCurr));
+            continue;
+        }
+        if(!nums.has(s[i]) && !opClo.has(s[i])) {
+            let wrdCurr = "";
+            while(!nums.has(s[i]) && !opClo.has(s[i]) && i < s.length) {
+                wrdCurr = wrdCurr + s[i];
+                i++;
+            }
+            i--;
+            wrds.push(wrdCurr);
+            continue;
+        }
+        if(openClose === 0 && wrds.length) {
+            decoded = decoded + (solve(wrds, rep));
+            rep = [];
+            wrds = [];
+
+        }
+    }
+    if(wrds.length) {
+        decoded = decoded + (solve(wrds, rep));
+    }
+    return decoded
+};
