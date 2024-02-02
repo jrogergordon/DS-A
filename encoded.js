220a2979127722f9de9278b2d515e4a90611f584
Jacob <jrogergordon@gmail.com>
2024-02-02 17:15:30 -0500

    base64 encoded

---
+++ encoded.js
@@ -0,0 +1,13 @@
+let decode = function(code) {
+    var fs = require("fs");
+    var text = fs.readFileSync(code).toString('utf-8').split("\n");;
+    let ans = [];
+    for(let i = 0; i < text.length; i++) {
+        text[i] = text[i].replace(`\r`, ''); 
+        text[i] = (Buffer.from(text[i], 'base64').toString()); 
+        ans.push(text[i]);
+    }
+    return ans.join(" ")
+}
+let p = decode("./encoded.txt");
+console.log(p);
